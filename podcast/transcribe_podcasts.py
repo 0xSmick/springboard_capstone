02c236950f2e65c3bf85d57c9e6969f9443f3bcd
Sheldon Smickley <sheldon.smickley@gmail.com>
2016-06-02 17:54:09 -0400

    added error exceptions to transcribe_podcasts.py

--- podcast/transcribe_podcasts.py
+++ podcast/transcribe_podcasts.py
@@ -20,42 +20,49 @@ r = sr.Recognizer()
 def transcribe_mp3(AUDIO_FILENAME, AUDIO_SEGMENT_SECONDS):
     output_file_name = "{}_translation.txt".format(AUDIO_FILENAME)
     #fuction to transform mp3 file to wav for transcription
-    
-    def transform_mp3_wav(AUDIO_FILENAME, AUDIO_SEGMENT_SECONDS):
-        filename = AUDIO_FILENAME.replace('.mp3','')
-        with open(AUDIO_FILENAME):
-            audio = AudioSegment.from_mp3(AUDIO_FILENAME)
-            xs = 0
-            while xs < audio.duration_seconds:
-                ys = min(xs + AUDIO_SEGMENT_SECONDS, ceil(audio.duration_seconds))
-                fname = str(xs).rjust(5, '0') + '-' + str(ys).rjust(5, '0') + '.wav'
-                audio[xs*1000:ys*1000].export(os.getcwd() + '/' + filename + fname, format='wav')
-                print("Saved", fname)
-                xs = ys
-    transform_mp3_wav(AUDIO_FILENAME, 300)
-    wav_filename = AUDIO_FILENAME.replace('.mp3','.wav')
-    wav_list = glob.glob('*.wav')
-    wav_list = filter(lambda x: '.mp3' not in x, wav_list)
-    trans_list = []
-    transcription = None
-    for wav_file in wav_list: 
-        print 'transcribing: ' + wav_file
-        with sr.AudioFile(wav_file) as source:
-            audio = r.record(source)
-            transcription = r.recognize_sphinx(audio)
-            print 'transcription completed'
-        trans_list.extend(transcription)
-        
-    transcription = ''.join(trans_list)
+    try:
+        def transform_mp3_wav(AUDIO_FILENAME, AUDIO_SEGMENT_SECONDS):
+            filename = AUDIO_FILENAME.replace('.mp3','')
+            with open(AUDIO_FILENAME):
+                audio = AudioSegment.from_mp3(AUDIO_FILENAME)
+                xs = 0
+                while xs < audio.duration_seconds:
+                    ys = min(xs + AUDIO_SEGMENT_SECONDS, ceil(audio.duration_seconds))
+                    fname = str(xs).rjust(5, '0') + '-' + str(ys).rjust(5, '0') + '.wav'
+                    audio[xs*1000:ys*1000].export(os.getcwd() + '/' + filename + fname, format='wav')
+                    print("Saved", fname)
+                    xs = ys
+        transform_mp3_wav(AUDIO_FILENAME, 300)
+        wav_filename = AUDIO_FILENAME.replace('.mp3','.wav')
+        wav_list = glob.glob('*.wav')
+        wav_list = filter(lambda x: '.mp3' not in x, wav_list)
+        trans_list = []
+        transcription = None
+        for wav_file in wav_list: 
+            print 'transcribing: ' + wav_file
+            with sr.AudioFile(wav_file) as source:
+                audio = r.record(source)
+                transcription = r.recognize_sphinx(audio)
+                print 'transcription completed'
+            trans_list.extend(transcription)
+            
+        transcription = ''.join(trans_list)
+    except:
+        return 'error'
     
     for f in wav_list:
         os.remove(f)
     
     file = open(output_file_name,'w')
     file.write(transcription)
-    file.close()
-
-for mp3_file in os.listdir(os.getcwd()):
-    print 'beginning transcription: ',mp3_file
-    transcribe_mp3(mp3_file, 300)
-    print 'transcription complete: ',mp3_file
+    file.close()    
+    
+for dirName, subdirList, fileList in os.walk(os.getcwd()):
+    os.chdir(dirName)
+    for fname in fileList:
+        if '.mp3' in fname:
+            print 'beginning transcription: ',fname
+            transcribe_mp3(fname, 300)
+            print 'transcription complete: ',fname
+        else:
+            print 'false'
