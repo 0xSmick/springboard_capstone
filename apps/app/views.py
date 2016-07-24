a790d52ba6f474edf12bdef988a2d819f0e9b238
Sheldon Smickley <sheldon.smickley@gmail.com>
2016-07-23 21:31:25 -0400

    refactor to use gensim, a lot faster

--- apps/app/views.py
+++ apps/app/views.py
@@ -1,20 +1,22 @@
 from app import app
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+from .forms import LoginForm
 import pandas as pd
 import numpy as np
 import os
 import glob
-import nltk.data
-import nltk, re, pprint
-from nltk import word_tokenize
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import linear_kernel
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
-from collections import Counter
-from sklearn.metrics.pairwise import linear_kernel
-from .forms import LoginForm
+from gensim import corpora, models, similarities
+import gensim
 import sqlite3
+
+dictionary = corpora.Dictionary.load('words.dict')
+corpus = corpora.MmCorpus('corpus.mm')
+tfidf = gensim.models.tfidfmodel.TfidfModel.load('tfidf_model')
+lsi = gensim.models.lsimodel.LsiModel.load('model.lsi')
+index = similarities.MatrixSimilarity.load('corpus.index')
+corpus_tfidf = tfidf[corpus]
+corpus_lsi = lsi[corpus_tfidf]
 
 def connect_db():
 	return sqlite3.connect('/Users/sheldon/podcasts/test.db')
@@ -26,29 +28,12 @@ def create_df_object():
 
 df = create_df_object()
 stop = set(stopwords.words('english'))
-tf = TfidfVectorizer(stop_words=stop)
-tfidf_matrix = tf.fit_transform(df['transcribed'])
-copy_matrix = tf.transform(df['transcribed'])
-cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
 @app.route('/index')
 @app.route('/', methods = ['GET','POST'])
 def main():
 	if request.method == "POST":
 		query = request.form.get('search','default value')
-		trans_query = query.lower()
-		trans_query = query.split()
-		tfidf_matrix_test = tf.fit_transform(trans_query)
-		tfidf_matrix_train = tf.transform(df['transcribed'])
-		tfidf_matrix_train.todense()
-		tfidf_matrix_test.todense()
-		query_similarities = linear_kernel(tfidf_matrix_test, tfidf_matrix_train)
-		query_similarities = query_similarities.argsort()[0][::-1]
-		pod_dict = dict(zip(range(0, len(query_similarities)),query_similarities))
-		pod_dict = pd.DataFrame({'rank':pod_dict.keys()}, index=pod_dict.values())
-		related_podcasts_df = pd.DataFrame.join(pod_dict, df, how='inner')
-	  	final_df = related_podcasts_df.sort_values('rank')[1:11][['rank','episode','series']]
-		related_podcasts = final_df['episode']
 		return redirect('/search/{}'.format(query))
 	else:
 		conn = sqlite3.connect('/Users/sheldon/podcasts/test.db')
@@ -78,32 +63,36 @@ def register():
 
 @app.route('/related_podcasts/<int:podcast_id>')
 def show_related_podcasts(podcast_id):
-		conn = sqlite3.connect('/Users/sheldon/podcasts/test.db')
-		c = conn.cursor()
-		cur = c.execute('select "index", episode, series from podcast where "index" ={}'.format(podcast_id))
-		db_request = [dict(index=row[0], episode=row[1], series=row[2]) for row in cur.fetchall()]
-		
-		related_pod_index = cosine_similarities.argsort()[podcast_id][::-1]
-		pod_dict = dict(zip(range(0, len(related_pod_index)),related_pod_index))
-		pod_dict = pd.DataFrame({'rank':pod_dict.keys()}, index=pod_dict.values())
-		related_podcasts_df = pd.DataFrame.join(pod_dict, df, how='inner')
-		final_df = related_podcasts_df.sort_values('rank')[1:11][['rank','episode','series']]
-		related_podcasts = final_df['episode']
-		return render_template('related_podcasts.html',original_title=db_request[0]['episode'], data=related_podcasts)
+	conn = sqlite3.connect('/Users/sheldon/podcasts/test.db')
+	c = conn.cursor()
+	cur = c.execute('select "index", episode, series from podcast where "index" ={}'.format(podcast_id))
+	db_request = [dict(index=row[0], episode=row[1], series=row[2]) for row in cur.fetchall()]
+	def get_related_podcasts(index):
+
+		def getKey(item):
+			return item[1]
+
+		corpus = corpus_lsi[index]
+		corpus = sorted(corpus, key=getKey, reverse=True)[0:10]
+		related_df = pd.DataFrame(corpus,columns=['index','score'])
+		final_df = pd.merge(related_df, df, on='index')[['index','episode','score','series']]
+		return final_df
+	final_df = get_related_podcasts(podcast_id)
+	related_podcasts = final_df['episode']
+	return render_template('related_podcasts.html',original_title=db_request[0]['episode'], data=related_podcasts)
 
 @app.route('/search/<string:query>')
-def show_related_podcast(query):
-		trans_query = query.lower()
-		trans_query = query.split()
-		tfidf_matrix_test = tf.fit_transform(trans_query)
-		tfidf_matrix_train = tf.transform(df['transcribed'])
-		tfidf_matrix_train.todense()
-		tfidf_matrix_test.todense()
-		query_similarities = linear_kernel(tfidf_matrix_test, tfidf_matrix_train)
-		query_similarities = query_similarities.argsort()[0][::-1]
-		pod_dict = dict(zip(range(0, len(query_similarities)),query_similarities))
-		pod_dict = pd.DataFrame({'rank':pod_dict.keys()}, index=pod_dict.values())
-		related_podcasts_df = pd.DataFrame.join(pod_dict, df, how='inner')
-	  	final_df = related_podcasts_df.sort_values('rank')[1:11][['rank','episode','series']]
-		related_podcasts = final_df['episode']
-		return render_template('related_podcasts_to_query.html',original_query=query, data=related_podcasts)
+def show_related_podcast_query(query):
+	trans_query = query.lower()
+	trans_query = query.split()
+	def get_related_podcasts_query(query):
+	    vec_box = dictionary.doc2bow(query.split())
+	    vec_lsi = lsi[vec_box]
+	    sims = index[vec_lsi]
+	    sims = sorted(enumerate(sims), key=lambda item: -item[1])[0:10]
+	    related_df = pd.DataFrame(sims,columns=['index','score'])
+	    final_df = pd.merge(related_df, df, on='index')[['index','episode','score','series']]
+	    return final_df
+	related_podcasts = get_related_podcasts_query(query)
+	related_podcasts = related_podcasts['episode']
+	return render_template('related_podcasts_to_query.html',original_query=query, data=related_podcasts)
---
+++ apps/podcasts.mm
@@ -0,0 +1,2 @@
+%%MatrixMarket matrix coordinate real general
+151 0 0                                           
